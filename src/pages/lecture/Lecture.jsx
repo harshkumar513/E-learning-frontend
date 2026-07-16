1230603b1b64ae4d24f83cf108d53159b7bfa92d
harshkumar513 <rajputharsh2323@gmail.com>
2026-07-16 21:58:13 +0530

    initial commit

--- src/pages/lecture/Lecture.jsx
+++ src/pages/lecture/Lecture.jsx
@@ -3,7 +3,7 @@ import "./lecture.css";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { server } from "../../main";
-import Loading from "../../components/loading/Loading";
+import Loading from "../../components/Loading/Loading";
 import toast from "react-hot-toast";
 import { TiTick } from "react-icons/ti";
 
